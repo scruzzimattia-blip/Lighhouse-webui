b26417fd7871cbbc2e809c6e3bd812fc3d69d8f9
Mattia <mattia@scruzzi.com>
2026-03-31 22:02:10 +0200

    fix: correct TemplateResponse call for newer Starlette version

--- app/main.py
+++ app/main.py
@@ -11,7 +11,7 @@ templates = Jinja2Templates(directory="app/templates")
 
 @app.get("/", response_class=HTMLResponse)
 async def read_item(request: Request):
-    return templates.TemplateResponse("index.html", {"request": request})
+    return templates.TemplateResponse(request, "index.html")
 
 @app.get("/api/containers")
 async def get_containers():
